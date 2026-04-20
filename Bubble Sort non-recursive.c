e523a4eca0e852a6c84acaa66e2e25f66204a144
JatinAgarwal270907 <jatinagarwal2707@gmail.com>
2026-04-20 21:39:19 +0530

    Create Bubble Sort non-recursive.c

---
+++ Bubble Sort non-recursive.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main()
+{
+    int n, a[10], i, j, temp;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+  
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n - i - 1; j++)
+        {
+            if(a[j] > a[j+1])
+            {
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted elements are:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
